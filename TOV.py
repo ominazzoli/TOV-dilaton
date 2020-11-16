7699962f5de3da5d674226d60efcb42949d27c15
Olivier Minazzoli <olivier.minazzoli@ligo.org>
2020-11-16 11:51:16 +0100

    add new version with new figures

--- TOV.py
+++ TOV.py
@@ -4,10 +4,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg as npla
+from scipy.integrate import solve_ivp
+from scipy.integrate import cumtrapz as integcum
+from scipy.integrate import trapz as integ
 
 c2 = cst.c**2
 kappa = 8*np.pi*cst.G/c2**2
 k = 1.475*10**(-3)*(cst.fermi**3/(cst.eV*10**6))**(2/3)*c2**(5/3)
+massSun = 1.989*10**30
 
 #Equation of state
 def PEQS(rho):
@@ -72,8 +76,8 @@ def f2(r, P, m, Psi, Phi, option):
     B = 4*np.pi*(-D00(r, P, m, Psi, Phi, option)/(kappa*c2))*r**2
     return A+B
 
-#Equation for dPhi/dr
-def f3(r, P, m, Psi, Phi, option):
+#Equation for dPsi/dr
+def f4(r, P, m, Psi, Phi, option, dilaton_active):
     ADOTA = adota(r, P, m, Psi, Phi)
     BDOTB = bdotb(r, P, m, Psi, Phi, option)
     rho = RhoEQS(P)
@@ -81,183 +85,176 @@ def f3(r, P, m, Psi, Phi, option):
     T = -c2*rho + 3*P
     A = (-Psi/2)*(ADOTA-BDOTB+4/r)
     B = b(r,m)*kappa*Phi**(1/2)*(T-Lm)/3
-    return A+B
+    if dilaton_active:
+        return A+B
+    else:
+        return 0
 
-#Equation for dPsi/dr
-def f4(r, P, m, Psi, Phi, option):
-    return Psi
+#Equation for dPhi/dr
+def f3(r, P, m, Psi, Phi, option, dilaton_active):
+    if dilaton_active:
+        return Psi
+    else:
+        return 0
+
+#Define for dy/dr
+def dy_dr(r, y, option, dilaton_active):
+    P, M, Phi, Psi = y
+    dy_dt = [f1(r, P, M, Psi, Phi, option), f2(r, P, M, Psi, Phi, option),f3(r, P, M, Psi, Phi, option, dilaton_active),f4(r, P, M, Psi, Phi, option, dilaton_active) ]
+    return dy_dt
+
+#Define for dy/dr out of the star
+def dy_dr_out(r, y, P, option, dilaton_active):
+    M, Phi, Psi = y
+    dy_dt = [f2(r, P, M, Psi, Phi, option),f3(r, P, M, Psi, Phi, option, dilaton_active),f4(r, P, M, Psi, Phi, option, dilaton_active) ]
+    return dy_dt
 
 class TOV():
 
-    def __init__(self, initRadius, initDensity, initPsi, initPhi, radiusStep, option, dilaton):
+    def __init__(self, initDensity, initPsi, initPhi, radiusMax_in, radiusMax_out, Npoint, EQS_type, dilaton_active, log_active):
 #Init value
         self.initDensity = initDensity
         self.initPressure = PEQS(initDensity)
         self.initPsi = initPsi
         self.initPhi = initPhi
         self.initMass = 0
-        self.initRadius = initRadius
+        self.option = EQS_type
+        self.dilaton_active = dilaton_active
+        self.log_active = log_active
+
 #Computation variable
-        self.limitCompute = 5000
-        self.radiusStep = radiusStep
+        self.radiusMax_in = radiusMax_in
+        self.radiusMax_out = radiusMax_out
+        self.Npoint = Npoint
 #Star data
+        self.Nstar = 0
         self.massStar = 0
+        self.massADM = 0
         self.pressureStar = 0
         self.radiusStar = 0
-        self.stepStar = 0
-        self.LambdaStar = 0
-#Output vector
+#Output data
         self.pressure = 0
         self.mass = 0
         self.Phi = 0
         self.Psi = 0
-        self.metric00 = 0
-        self.metric11 = 0
         self.radius = 0
-
-        self.PhiInf = 0
-        self.option = option
-        self.dilaton = dilaton
+        self.g_tt = 0
+        self.g_rr = 0
+        self.phi_inf = 0
 
     def Compute(self):
-# Initialisation ===================================================================================
-        dr = self.radiusStep
-        n = 0
-        P =        [self.initPressure]
-        m =        [self.initMass]
-        Psi =      [self.initPsi]
-        Phi =      [self.initPhi]
-        r =        [self.initRadius]
-        metric11 = [b(r[0],m[0])]
-        metric00 = [1]      # Coordinate time as proper time at center
-        # Inside the star----------------------------------------------------------------------------
-        while(P[n]>10**26):
-            if(n == self.limitCompute):
-                break
-                print('end')
-            else:
-                P.append(     P[n]   + dr*f1(r[n], P[n], m[n], Psi[n], Phi[n], self.option ))
-                m.append(     m[n]   + dr*f2(r[n], P[n], m[n], Psi[n], Phi[n], self.option ))
-                if self.dilaton:
-                    Phi.append(   Phi[n] + dr*f4(r[n], P[n], m[n], Psi[n], Phi[n], self.option ))
-                    Psi.append(   Psi[n] + dr*f3(r[n], P[n], m[n], Psi[n], Phi[n], self.option ))
-                else:
-                    Phi.append(   Phi[n] )
-                    Psi.append(   Psi[n] )
-                metric11.append(b(r[n], m[n]))
-                metric00.append(metric00[n] + dr*metric00[n]*adota(r[n], P[n], m[n], Psi[n], Phi[n]))
-                n = n+1
-                r.append(self.initRadius+n*dr)
-        P.pop()
-        m.pop()
-        Psi.pop()
-        Phi.pop()
-        r.pop()
-        metric11.pop()
-        metric00.pop()
-        n = n-1
-        self.pressure = P #star pressure
-        self.mass = m #star mass
-        self.stepStar = n #index where code stop at 0 pressure
-
-        # Outside the star--------------------------------------------------------------------------
-        while(n<self.limitCompute):
-            if self.dilaton:
-                Phi.append( Phi[n] + dr*f4(r[n], 0, m[n], Psi[n], Phi[n], self.option ))
-                Psi.append( Psi[n] + dr*f3(r[n], 0, m[n], Psi[n], Phi[n], self.option ))
-            else:
-                Phi.append( Phi[n] )
-                Psi.append( Psi[n] )
-            P.append( P[n] + dr*f1(r[n], P[n], m[n], Psi[n], Phi[n], self.option ))
-            m.append( m[n] + dr*f2(r[n], 0, m[n], Psi[n], Phi[n], self.option ))
-            metric11.append(b(r[n], m[n]))
-            metric00.append(metric00[n] + dr*metric00[n]*adota(r[n],0, m[n], Psi[n], Phi[n]))
-            n = n+1
-            r.append(self.initRadius+n*dr)
-
-        # Star property
-        self.massStar = self.mass[self.stepStar]
-        self.radiusStar = r[self.stepStar]
-        self.pressureStar = self.pressure[self.stepStar]
-        self.Psi = Psi
-        self.Phi = Phi
-        self.radius = r
-        self.metric11 = metric11
-        self.metric00 = metric00
-
-    def ComputeGR(self):
-        dr = self.radiusStep
-        n = 0 #Integration parameter
-        P =        [self.initPressure]
-        m =        [self.initMass]
-        r =        [self.initRadius]
-        metric11 = [b(r[0],m[0])]
-        metric00 = [1]      # Coordinate time as proper time at center
-        while(P[n]>10**26):
-            if(n == self.limitCompute):
-                break
-                print('end')
-            else:
-                P.append( P[n] + dr*f1(r[n], P[n], m[n], 0, 1, self.option ))
-                m.append( m[n] + dr*f2(r[n], P[n], m[n], 0, 1, self.option ))
-                metric11.append(b(r[n], m[n]))
-                metric00.append(metric00[n] + dr*metric00[n]*adota(r[n],0, m[n], 0, 1))
-                n = n+1
-                r.append(self.initRadius+n*dr)
-        P.pop()
-        m.pop()
-        r.pop()
-        metric00.pop()
-        metric11.pop()
-        n = n-1
-        self.massStar = m[n]
-        self.radiusStar = r[n]
-        self.pressureStar = P[n]
-        self.pressure = P
-        self.mass = m
-        self.metric00 = metric00
-        self.metric11 = metric11
+        if self.log_active:
+            print('===========================================================')
+            print('SOLVER INSIDE THE STAR')
+            print('===========================================================\n')
+            print('Initial density: ', self.initDensity, ' MeV/fm^3')
+            print('Initial pressure: ', self.initPressure/10**12, ' GPa')
+            print('Initial mass: ', self.initMass/massSun, ' solar mass')
+            print('Initial phi: ', self.initPhi)
+            print('Initial psi: ', self.initPsi)
+            print('Number of points: ', self.Npoint)
+            print('Radius max: ', self.radiusMax_in/1000, ' km')
+        y0 = [self.initPressure,self.initMass,self.initPhi,self.initPsi]
+        if self.log_active:
+            print('y0 = ', y0,'\n')
+        r = np.linspace(0.01,self.radiusMax_in,self.Npoint)
+        if self.log_active:
+            print('radius min ',0.01)
+            print('radius max ',self.radiusMax_in)
+        sol = solve_ivp(dy_dr, [0.01, self.radiusMax_in], y0, t_eval=r ,args=(self.option,self.dilaton_active))
+        # condition for Pressure = 0
+        if sol.t[-1]<self.radiusMax_in:
+            self.pressure = sol.y[0]
+            self.mass = sol.y[1]
+            self.Phi = sol.y[2]
+            self.Psi = sol.y[3]
+            self.radius = sol.t
+            # Value at the radius of star
+            self.massStar = self.mass[-1]
+            self.radiusStar = self.radius[-1]
+            self.pressureStar = self.pressure[-1]
+            if self.log_active:
+                print('Star radius: ', self.radiusStar/1000, ' km')
+                print('Star Mass: ', self.massStar/massSun, ' solar mass')
+                print('Star pressure: ', self.pressureStar, ' Pa\n')
+                print('===========================================================')
+                print('SOLVER OUTSIDE THE STAR')
+                print('===========================================================\n')
+            y0 = [self.massStar, self.Phi[-1],self.Psi[-1]]
+            if self.log_active:
+                print('y0 = ', y0,'\n')
+            r = np.linspace(self.radiusStar,self.radiusMax_out,self.Npoint)
+            if self.log_active:
+                print('radius min ',self.radiusStar)
+                print('radius max ',self.radiusMax_out)
+            sol = solve_ivp(dy_dr_out, [self.radiusStar, self.radiusMax_out], y0, t_eval=r, args=(0,self.option,self.dilaton_active))
+            self.pressure = np.concatenate([self.pressure, np.zeros(self.Npoint)])
+            self.mass = np.concatenate([self.mass, sol.y[0]])
+            self.Phi = np.concatenate([self.Phi, sol.y[1]])
+            self.Psi = np.concatenate([self.Psi,  sol.y[2]])
+            self.radius = np.concatenate([self.radius, sol.t])
+            self.phi_inf = self.Phi[-1]
+            if self.log_active:
+                print('Phi at infinity ', self.phi_inf)
+            # Compute metrics
+            self.g_rr = b(self.radius, self.mass)
+            a_dot_a = adota(self.radius, self.pressure, self.mass, self.Psi, self.Phi)
+            self.g_tt = np.exp(np.concatenate([[0.0], integcum(a_dot_a,self.radius)])-integ(a_dot_a,self.radius))
+            self.massADM = self.mass[-1]
+            if self.log_active:
+                print('===========================================================')
+                print('END')
+                print('===========================================================\n')
+        else:
+            print('Pressure=0 not reached')
 
 
-    def PlotEvolution(self):
-        plt.figure()
-        plt.plot([x/10**3 for x in self.radius[0:self.stepStar*2]], [x for x in self.pressure[0:self.stepStar*2]])
+    def ComputeTOV(self):
+        """
+        ComputeTOV is the function to consider in order to compute "physical" quantities. It takes into account phi_inf->1 r->ininity
+        """
+        self.Compute()
+        self.initPhi = self.initPhi/self.phi_inf
+        self.Compute()
+
+    def Plot(self):
+        plt.subplot(221)
+        plt.plot([x/10**3 for x in self.radius], [x for x in self.pressure])
         plt.xlabel('Radius r (km)')
         plt.title('Pressure P (Pa)', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
-        plt.figure()
-        plt.plot([x/10**3 for x in self.radius[0:self.stepStar*2]], [x/(1.989*10**30) for x in self.mass[0:self.stepStar*2]])
+        plt.subplot(222)
+        plt.plot([x/10**3 for x in self.radius], [x/massSun for x in self.mass])
         plt.xlabel('Radius r (km)')
         plt.title('Mass $M/M_{\odot}$', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
-        plt.figure()
-        plt.plot([x/10**3 for x in self.radius[0:self.stepStar*20]], self.Phi[0:self.stepStar*20])
+        plt.subplot(223)
+        plt.plot([x/10**3 for x in self.radius], self.Phi)
         plt.xlabel('Radius r (km)')
         plt.title('Dilaton field Φ', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
-        plt.figure()
-        plt.plot([x/10**3 for x in self.radius[0:self.stepStar*20]], self.Psi[0:self.stepStar*20])
+        plt.subplot(224)
+        plt.plot([x/10**3 for x in self.radius], self.Psi)
         plt.xlabel('Radius r (km)')
         plt.title('Ψ (derivative of Φ)', fontsize=12)
-
-        plt.figure()
-        ax3 = plt.subplot(1,2,1)
-        plt.plot([x/10**3 for x in self.radius], self.metric00)
-        plt.xlabel('Radius r (km)')
-        plt.ylabel('a')
-
-        ax4 = plt.subplot(1,2,2)
-        plt.plot([x/10**3 for x in self.radius], self.metric11)
-        plt.xlabel('Radius r (km)')
-        plt.ylabel('b')
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
         plt.show()
 
-    def Phi_infini(self):
-        return self.Phi[self.stepStar*10]
+    def PlotMetric(self):
+        plt.subplot(121)
+        plt.plot([x/10**3 for x in self.radius], self.g_tt)
+        plt.xlabel('Radius r (km)')
+        plt.title('g_tt', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
 
-    def Phi_r(self):
-        M = np.zeros(len(self.Phi))
-        for i in range(len(self.Phi)):
-            M[i] = self.Phi[i]-self.initPhi
-        plt.plot([x/10**3 for x in self.radius[:]], M)
+        plt.subplot(122)
+        plt.plot([x/10**3 for x in self.radius], self.g_rr)
+        plt.xlabel('Radius r (km)')
+        plt.title('g_rr', fontsize=12)
+        plt.axvline(x=self.radiusStar/10**3, color='r')
+
+        plt.show()
